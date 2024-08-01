00790153c258aeaba503b8617fd48b4e9b9c5702
ricidkumbara <ricidkumbara@gmail.com>
2024-08-01 23:18:26 +0700

    update

---
+++ 21-bufio.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	input := strings.NewReader("this is long\ntext paragraf\n")
+	reader := bufio.NewReader(input)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(string(line))
+	}
+
+	writer := bufio.NewWriter(os.Stdout)
+	_, _ = writer.WriteString("hello world\n")
+	_, _ = writer.WriteString("hello world\n")
+	writer.Flush()
+}
